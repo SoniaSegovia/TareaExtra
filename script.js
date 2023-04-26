207914b007daec393951d923e4196ab86635921e
SoniaSegovia <95039528+SoniaSegovia@users.noreply.github.com>
2023-04-26 12:31:29 -0600

    Guardar cambios

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const templateHeader = document.querySelector("#templateHeader")
 let carritoArray = []
 
 document.addEventListener("click", (e) => {
-    if (e.target.matches(".card button")) {
+    if (e.target.matches("article .card button")) {
         agregarCarrito(e)
     }
 
@@ -20,6 +20,10 @@ document.addEventListener("click", (e) => {
 
     if (e.target.matches("#btnDisminuir")) {
         btnDisminuir(e)
+    }
+    //al hacer click al boton finalizar compra llamo la funcion de finalizarcompra()
+    if(e.target.matches("#btnFinalizar")){
+        finalizarCompra()
     }
 })
 
@@ -92,12 +96,42 @@ const mostrarHeaderFooter = (
     header.deleteTHead()
     footer.textContent = ""
 
-    const total = carritoArray.reduce((acc, current) => acc + current.precio * current.cantidad, 0)
+    const total = carritoArray.reduce((acc,current) => acc + current.precio * current.cantidad, 0)
 
     const cloneHeader = templateHeader.content.cloneNode(true)
     const cloneFooter = templateFooter.content.cloneNode(true)
     cloneFooter.querySelector("p span").textContent = total
-
     header.appendChild(cloneHeader)
     footer.appendChild(cloneFooter)
+}
+
+const finalizarCompra = () =>{
+    //recorro los botones como un array y les asigno a cada uno con la propiedad .className el nombre de la clase disabled para deshabilitarlos
+    for(var i = 0; i < botones.length; i++)
+    botones[i].className += " disabled";
+
+    //obtengo todos los botones de aumentar y disminuir como un array 
+    const btnAumentarDisminuir = carrito.querySelectorAll("button")
+
+    //recorro los botones aumentar y disminuir y les asigno a cada uno con la propiedad .className el nombre de la clase disabled para deshabilitarlos 
+    for(var i = 0; i < btnAumentarDisminuir.length; i++){
+    btnAumentarDisminuir[i].className += " disabled";
+    }
+    
+    //obtengo el total y la cantidad de la compra  
+    const totalCompra = carritoArray.reduce((acc,current) => acc + current.precio * current.cantidad, 0)
+    const cantidadCompra = carritoArray.reduce((acc,current) =>acc+current.cantidad ,0)
+
+    // duplico el template para insertar el total y cantidad al finalizar la compra
+    const clone = template.content.cloneNode(true)
+    clone.querySelector("#cantidad").textContent = cantidadCompra
+    clone.querySelector("#total span").textContent = totalCompra
+    //creo el html con el fragment
+    fragment.appendChild(clone)
+    //se crea la fila en el tbody
+    carrito.appendChild(fragment)
+    //obtengo los botones ultimos aunmentar y disminuir en totales y cantidad para eliminarlo 
+    carrito.children[carrito.children.length-1].children[3].innerHTML=""
+    //Deshabilito el boton finalizar compra
+    document.getElementById("btnFinalizar").className+=" disabled"
 }
